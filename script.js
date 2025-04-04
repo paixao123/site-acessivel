6af46d11cc10a958f479245186212c7fc55bb5bd
paixao123 <paixao.anna@escola.pr.gov.br>
2025-04-04 14:02:09 +0000

    adicionando

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 document.addEventListener('DOMContentLoaded', function () {
 
-    const aumentaFonteBotao = document.getElementById('aumentar-fonte');
-    const diminuiFonteBotao = document.getElementById('diminuir-fonte');
+    const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade');
+    const opcoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade');
+
+    botaoDeAcessibilidade.addEventListener('click', function () {
+        botaoDeAcessibilidade.classList.toggle('rotacao-botao');
+        opcoesDeAcessibilidade.classList.toggle('apresenta-lista');
+    })
 
     let tamanhoAtualFonte = 1;
 
